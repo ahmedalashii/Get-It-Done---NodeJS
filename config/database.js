b2db0e0f5d3c880615bbc42657c4a6a21698a68b
ahmed_alashii <you@example.com>
2023-05-13 23:03:15 +0300

    ..

--- config/database.js
+++ config/database.js
@@ -4,11 +4,20 @@ const mongoose = require('mongoose');
 const { ATLAS_DB_STRING } = process.env;
 
 /*
-    mongodb vs mongoose (pros and cons):
+    ? mongodb vs mongoose (Pros & Cons):
 
     Using mongoose, a user can define the schema for the documents in a particular collection. It provides a lot of convenience in the creation and management of data in MongoDB. On the downside, learning mongoose can take some time, and has some limitations in handling schemas that are quite complex.
 
     However, if your collection schema is unpredictable, or you want a Mongo-shell like experience inside Node.js, then go ahead and use the mongodb driver. It is the simplest to pick up. The downside here is that you will have to write larger amounts of code for validating the data, and the risk of errors is higher.
+
+    ---------- 
+
+    If you don't want to use any ORM for your data models then you can also use native driver mongo.js: https://github.com/mongodb/node-mongodb-native.
+
+    Mongoose is one of the orm's who give us functionality to access the mongo data with easily understandable queries.
+
+    Mongoose plays as a role of abstraction over your database model.
+
 */
 
 
